d89555bd17424eee2bdc0bf541fa17d4aa5e4822
Petrs911 <petrs911@gmail.com>
2021-12-01 19:00:57 +0200

    added svg icons

---
+++ lib/presentation/theme/icons.dart
@@ -0,0 +1,11 @@
+class WebDashboardIcons {
+  static const String bell = 'assets/icons/bell.svg';
+
+  static const String cup = 'assets/icons/cup.svf';
+
+  static const String homeTeamNs = 'assets/icons/home_team_ns.svg';
+
+  static const String home = 'assets/icons/home.svg';
+
+  static const String stack = 'assets/icons/stack.svg';
+}
