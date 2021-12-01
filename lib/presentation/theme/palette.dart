08a4bb0a6b5f4b2bc96bb4199a921ac35000ddda
Petrs911 <petrs911@gmail.com>
2021-12-01 19:01:34 +0200

    added main colors of the app

---
+++ lib/presentation/theme/palette.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class WebDashboardPalette {
+  ///0xFFE5E5E5
+  static const Color background = Color(0xFFE5E5E5);
+
+  ///0xFF015DAB
+  static const Color brandBlue = Color(0xFF015DAB);
+
+  ///0xFF15224F
+  static const Color darkBlue = Color(0xFF15224F);
+
+  ///0xFFF26322
+  static const Color brandOrange = Color(0xFFF26322);
+
+  ///0xFF40465B
+  static const Color black = Color(0xFF40465B);
+
+  ///0xFF596282
+  static const Color darkGrey = Color(0xFF596282);
+
+  ///0xFFA7ACBD
+  static const Color grey = Color(0xFFA7ACBD);
+
+  ///0xFFEAECEF
+  static const Color lightGrey = Color(0xFFEAECEF);
+
+  ///0xFFFF6A6A
+  static const Color red = Color(0xFFFF6A6A);
+}
