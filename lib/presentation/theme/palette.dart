718cddce24ab202b49983c2618007415bafc941c
Petrs911 <petrs911@gmail.com>
2021-12-02 19:35:35 +0200

    added new colors

--- lib/presentation/theme/palette.dart
+++ lib/presentation/theme/palette.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
 
 class WebDashboardPalette {
-  ///0xFFE5E5E5
-  static const Color background = Color(0xFFE5E5E5);
+  ///0xFFF8F9F9
+  static const Color dirtyWhite = Color(0xFFF8F9F9);
+
+  ///0xFFF8F9F9
+  static const Color background = Color(0xFFF8F9F9);
+
+  ///0xFF5363F6
+  static const Color blue = Color(0xFF5363F6);
+
+  ///0xFF1C86DF
+  static const Color lightBlue = Color(0xFF1C86DF);
 
   ///0xFF015DAB
   static const Color brandBlue = Color(0xFF015DAB);
 
   ///0xFF15224F
   static const Color darkBlue = Color(0xFF15224F);
+
+  ///0xFF191C24
+  static const Color dark = Color(0xFF191C24);
+
+  ///0xFFEEE8F8
+  static const Color purple = Color(0xFFEEE8F8);
+
+  ///0xFFFF7537
+  static const Color orange = Color(0xFFFF7537);
+
+  ///0xFFFFD233
+  static const Color yellow = Color(0xFFFFD233);
+
+  ///0xFF4CD964
+  static const Color green = Color(0xFF4CD964);
 
   ///0xFFF26322
   static const Color brandOrange = Color(0xFFF26322);
