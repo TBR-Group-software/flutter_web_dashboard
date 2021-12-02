34e82343e3b022d3b1a4c509f23160054d8867d6
Petrs911 <petrs911@gmail.com>
2021-12-02 19:35:17 +0200

    added new icons

--- lib/presentation/theme/icons.dart
+++ lib/presentation/theme/icons.dart
@@ -1,11 +1,25 @@
 class WebDashboardIcons {
   static const String bell = 'assets/icons/bell.svg';
 
-  static const String cup = 'assets/icons/cup.svg';
+  static const String cupWhite = 'assets/icons/cup_white.svg';
 
-  static const String homeTeamNs = 'assets/icons/home_team_ns.svg';
+  static const String cupGrey = 'assets/icons/cup_grey.svg';
 
-  static const String home = 'assets/icons/home.svg';
+  static const String whiteLogo = 'assets/icons/white_logo.svg';
 
-  static const String stack = 'assets/icons/stack.svg';
+  static const String homeWhite = 'assets/icons/home_white.svg';
+
+  static const String homeGrey = 'assets/icons/home_grey.svg';
+
+  static const String stackWhite = 'assets/icons/stack_white.svg';
+
+  static const String stackGrey = 'assets/icons/stack_grey.svg';
+
+  static const String rocketOrange = 'assets/icons/rocket_orange.svg';
+
+  static const String chartPurple = 'assets/icons/сhart_purple.svg';
+
+  static const String newUserBlue = 'assets/icons/new_user_blue.svg';
+
+  static const String speedometerYellow = 'assets/icons/speedometer_yellow.svg';
 }
